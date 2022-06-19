a61ef0178d22da04f615d99438d265a19b0ed4ee
Ravisadilanka <raveeshadilanka1999@gmail.com>
2022-06-20 02:20:55 +0530

    Product

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ const Home = ({ products, bannerData}) => (
 
     <div className="products-heading">
       <h2>Best Selling Products</h2>
-      <p>Speakerd of many variations</p>
+      <p>Speaker there are many variations</p>
     </div>
 
     <div className='products-container'>
-      {products?.map((product)=>product.name)}
+      {products?.map((product)=><Product key={product._id} product={product} />)}
     </div>
 
-    <FooterBanner/>
+    <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </div>
   );
 
