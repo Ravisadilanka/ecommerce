68cb7caaa08f6c51e247444549a80a035185672c
Ravisadilanka <raveeshadilanka1999@gmail.com>
2022-06-27 01:33:45 +0530

    Stripe and Success Page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -9,17 +9,25 @@ import { useStateContext } from '../../context/StateContext'
 const ProductDetails = ({product,products}) => {
     const {image, name, details, price} = product;
     const [index, setIndex] = useState(0);
-    const {decQty, incQty, qty, onAdd} = useStateContext();
+    const {decQty, incQty, qty, onAdd, setShowCart} = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+
+        setShowCart(true);
+    }
+
     return (
         <div>
-            <div class="product-detail-container">
+            <div className="product-detail-container">
                 <div>
-                    <div class="image-container">
+                    <div className="image-container">
                         <img src={urlFor(image && image[index])} className = "product-detail-image"/>
                     </div>
-                    <div class="small-images-container">
+                    <div className="small-images-container">
                         {image?.map((item, i) => (
                             <img
+                            key={i}
                             src={urlFor(item)}
                             className={i === index ? 'small-image selected-image' : 'small-image'}
                             onMouseEnter={() => setIndex(i)}
@@ -28,9 +36,9 @@ const ProductDetails = ({product,products}) => {
                     </div>
                 </div>
 
-                <div class="product-detail-desc">
+                <div className="product-detail-desc">
                     <h1>{name}</h1>
-                    <div class="reviews">
+                    <div className="reviews">
                         <div>
                             <AiFillStar />
                             <AiFillStar />
@@ -45,25 +53,25 @@ const ProductDetails = ({product,products}) => {
                     <h4>Details: </h4>
                     <p>{details}</p>
                     <p className='price'>${price}</p>
-                    <div class="quantity">
+                    <div className="quantity">
                         <h3>Quantity:</h3>
                         <p className='quantity-desc'>
                             <span className='minus' onClick={decQty}><AiOutlineMinus/></span>
-                            <span className='num' onClick="">{qty}</span>
+                            <span className='num'>{qty}</span>
                             <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
                         </p>
                     </div>
-                    <div class="buttons">
+                    <div className="buttons">
                         <button type='button' className='add-to-cart' onClick={() => onAdd(product,qty)}> Add to Cart</button>
-                        <button type='button' className='buy-now'> Buy Now</button>
+                        <button type='button' className='buy-now' onClick={handleBuyNow}> Buy Now</button>
                     </div>
                 </div>
             </div>
 
-            <div class="maylike-products-wrapper">
+            <div className="maylike-products-wrapper">
                 <h2>You may also like</h2>
-                <div class="marquee">
-                    <div class="maylike-products-container track">
+                <div className="marquee">
+                    <div className="maylike-products-container track">
                         {products.map((item) => (
                             <Product key={item._id} product={item}/>
                         ))}
